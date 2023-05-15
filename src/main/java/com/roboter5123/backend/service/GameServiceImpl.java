84f22bb43ed6218f39d9e458ba04caba833b5263
roboter5123 <pascal.roboter@hotmail.com>
2023-05-15 20:10:58 +0200

    Added an implementation of gameservice

---
+++ src/main/java/com/roboter5123/backend/service/GameServiceImpl.java
@@ -0,0 +1,14 @@
+package com.roboter5123.backend.service;
+import com.roboter5123.backend.service.model.Command;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameServiceImpl implements GameService{
+
+
+
+    @Override
+    public void addCommand(Command command, String gameCode) {
+
+    }
+}
