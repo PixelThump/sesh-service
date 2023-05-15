22eedc36d70fec59e9ead911134ef1705d54897b
roboter5123 <pascal.roboter@hotmail.com>
2023-05-15 19:03:39 +0200

    Imported prototype from work

---
+++ src/test/java/com/roboter5123/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roboter5123.backend;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        assertEquals(true,true);
+    }
+
+}
