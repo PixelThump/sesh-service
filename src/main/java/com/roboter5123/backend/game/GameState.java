a04f4a74819e828815a8df7777504bd32a2f7f6c
roboter5123 <pascal.roboter@hotmail.com>
2023-05-16 21:50:17 +0200

    Comment

--- src/main/java/com/roboter5123/backend/game/GameState.java
+++ src/main/java/com/roboter5123/backend/game/GameState.java
@@ -1,6 +1,7 @@
 package com.roboter5123.backend.game;
 import java.util.Map;
 
+//TODO: Split this into 2 classes. One that actualy represents the gamestate internaly and one that gets sent to the frotnend
 public interface GameState<T>{
 
     Message<String> getJoinMessage();
