fb6b6acd03db21cd79ce9717790d20ec36048a9e
roboter5123 <pascal.roboter@hotmail.com>
2023-06-14 18:13:57 +0200

    Updated config files for updated dependencies

--- src/main/java/com/roboter5123/play/backend/webinterface/config/MiscConfig.java
+++ src/main/java/com/roboter5123/play/backend/webinterface/config/MiscConfig.java
@@ -1,10 +1,10 @@
 package com.roboter5123.play.backend.webinterface.config;
 import com.roboter5123.play.backend.game.api.GameFactory;
 import com.roboter5123.play.backend.game.implementation.GameFactoryImpl;
-import com.roboter5123.play.messaging.api.MessageBroadcaster;
-import com.roboter5123.play.messaging.api.StompMessageFactory;
-import com.roboter5123.play.messaging.implementation.MessageBroadcasterImpl;
-import com.roboter5123.play.messaging.implementation.StompMessageFactoryImpl;
+import com.roboter5123.play.backend.messaging.api.MessageBroadcaster;
+import com.roboter5123.play.backend.messaging.api.StompMessageFactory;
+import com.roboter5123.play.backend.messaging.implementation.MessageBroadcasterImpl;
+import com.roboter5123.play.backend.messaging.implementation.StompMessageFactoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
