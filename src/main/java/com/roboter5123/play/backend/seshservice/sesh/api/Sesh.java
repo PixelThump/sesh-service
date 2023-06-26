951451efbc7e47ad6f2e64b8a1e6dd0148e6c909
roboter5123 <pascal.roboter@hotmail.com>
2023-06-26 20:09:14 +0200

    Added a base clas for seshs to inherit from. It provides functionality that all seshs will need

--- src/main/java/com/roboter5123/play/backend/seshservice/sesh/api/Sesh.java
+++ src/main/java/com/roboter5123/play/backend/seshservice/sesh/api/Sesh.java
@@ -1,18 +1,19 @@
 package com.roboter5123.play.backend.seshservice.sesh.api;
 import com.roboter5123.play.backend.seshservice.messaging.model.Command;
+import com.roboter5123.play.backend.seshservice.sesh.exception.PlayerAlreadyJoinedException;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 public interface Sesh {
 
-    Map<String, Object> joinSesh(String playerName);
+    Map<String, Object> joinSesh(String playerName) throws PlayerAlreadyJoinedException;
 
     SeshType getSeshType();
 
     String getSeshCode();
 
-    void broadcast(Object joinCommand);
+    void broadcast(Object payload);
 
     void addCommand(Command command) throws UnsupportedOperationException;
 
---
+++ src/main/java/com/roboter5123/play/backend/seshservice/sesh/implementation/AbstractSeshBaseClass.java
@@ -0,0 +1,33 @@
+package com.roboter5123.play.backend.seshservice.sesh.implementation;
+import com.roboter5123.play.backend.seshservice.messaging.api.MessageBroadcaster;
+import com.roboter5123.play.backend.seshservice.sesh.api.Sesh;
+import com.roboter5123.play.backend.seshservice.sesh.api.SeshType;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public abstract class AbstractSeshBaseClass implements Sesh {
+
+    @Getter
+    private final SeshType seshType;
+    @Getter
+    private final String seshCode;
+
+    private final MessageBroadcaster broadcaster;
+    @Getter
+    protected LocalDateTime lastInteractionTime;
+
+    protected AbstractSeshBaseClass(String seshCode, MessageBroadcaster broadcaster, SeshType seshType) {
+
+        this.seshCode = seshCode;
+
+        this.broadcaster = broadcaster;
+        this.seshType = seshType;
+        this.lastInteractionTime = LocalDateTime.now();
+    }
+
+    @Override
+    public void broadcast(Object payload) {
+        this.broadcaster.broadcastSeshUpdate(this.seshCode, payload);
+    }
+}
