1e7d5f8ab5efc9864648dcc4b7dd1623d044aec5
roboter5123 <pascal.roboter@hotmail.com>
2023-06-30 19:36:13 +0200

    Added exceptions

--- src/main/java/com/roboter5123/play/backend/seshservice/sesh/api/Sesh.java
+++ src/main/java/com/roboter5123/play/backend/seshservice/sesh/api/Sesh.java
@@ -1,14 +1,17 @@
 package com.roboter5123.play.backend.seshservice.sesh.api;
 import com.roboter5123.play.backend.seshservice.messaging.model.Command;
+import com.roboter5123.play.backend.seshservice.sesh.exception.PlayerAlreadyJoinedException;
+import com.roboter5123.play.backend.seshservice.sesh.exception.SeshCurrentlyNotJoinableException;
+import com.roboter5123.play.backend.seshservice.sesh.exception.SeshIsFullException;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 public interface Sesh {
 
-    Map<String, Object> joinSeshAsHost();
+    Map<String, Object> joinSeshAsHost() throws PlayerAlreadyJoinedException ;
 
-    Map<String, Object> joinSeshAsController(String playerName);
+    Map<String, Object> joinSeshAsController(String playerName) throws SeshIsFullException, PlayerAlreadyJoinedException, SeshCurrentlyNotJoinableException;
 
     SeshType getSeshType();
 
