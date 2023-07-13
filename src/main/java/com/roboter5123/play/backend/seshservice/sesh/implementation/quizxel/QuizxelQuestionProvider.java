ae17ab423e8b0368650c82ce0a1af7449620c044
roboter5123 <pascal.roboter@hotmail.com>
2023-07-13 13:56:55 +0200

    Added a way to get current question instead of next question

--- src/main/java/com/roboter5123/play/backend/seshservice/sesh/implementation/quizxel/QuizxelQuestionProvider.java
+++ src/main/java/com/roboter5123/play/backend/seshservice/sesh/implementation/quizxel/QuizxelQuestionProvider.java
@@ -2,10 +2,32 @@ package com.roboter5123.play.backend.seshservice.sesh.implementation.quizxel;
 import com.roboter5123.play.backend.seshservice.sesh.implementation.quizxel.model.question.BuzzerQuestion;
 import com.roboter5123.play.backend.seshservice.sesh.implementation.quizxel.model.question.QuizxelQuestion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizxelQuestionProvider {
 
-    public QuizxelQuestion getNextQuestion(){
+    private final List<QuizxelQuestion> questions;
+    private int currentIndex;
 
-        return new BuzzerQuestion("Test Question Text", "Test Question Answer");
+    public QuizxelQuestionProvider() {
+
+        List<QuizxelQuestion> localQuestions = new ArrayList<>();
+        localQuestions.add(new BuzzerQuestion("Test Question Text 1", "Test Question Answer 1"));
+        localQuestions.add(new BuzzerQuestion("Test Question Text 2", "Test Question Answer 2"));
+        this.questions = localQuestions;
+        currentIndex = 0;
+    }
+
+    public QuizxelQuestion getCurrentQuestion(){
+
+        return questions.get(currentIndex);
+    }
+
+    public QuizxelQuestion getNextQuestion() {
+
+        currentIndex++;
+        if (currentIndex >= (questions.size())) currentIndex = 0;
+        return questions.get(currentIndex);
     }
 }
