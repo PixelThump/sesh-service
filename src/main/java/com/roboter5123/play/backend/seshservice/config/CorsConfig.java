84737398a602ace25d9f64026d36e49b33db2049
roboter5123 <pascal.roboter@hotmail.com>
2023-07-20 19:13:37 +0200

    Fixed cors hopefully

--- src/main/java/com/roboter5123/play/backend/seshservice/config/CorsConfig.java
+++ src/main/java/com/roboter5123/play/backend/seshservice/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
