309e7e73becb7663b2b675b37c8303c6da0d2e53
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-24 14:58:31 +0200

    Update QuizxelQuestionProvider.java
    Added more questions

--- src/main/java/com/roboter5123/play/backend/seshservice/sesh/implementation/quizxel/QuizxelQuestionProvider.java
+++ src/main/java/com/roboter5123/play/backend/seshservice/sesh/implementation/quizxel/QuizxelQuestionProvider.java
@@ -21,7 +21,13 @@ public class QuizxelQuestionProvider {
         localQuestions.add(new BuzzerQuestion("6. Was war denn der erste releaste mcu Film?", "Iron Man"));
         localQuestions.add(new BuzzerQuestion("7. Und der neueste?", "Guardians of the Galaxy Vol. 3"));
         localQuestions.add(new BuzzerQuestion("8. In Iron Man 2 gab es ein kameo von einem Billionär. Wie heißt er?", "Elon Musk"));
-        localQuestions.add(new BuzzerQuestion("9. Eine Letzte mcu frage noch. Für welchen Satz ist Vin diesels Character bekannt?", "I am Groot/ Ich bin Groot"));
+        localQuestions.add(new BuzzerQuestion("9. Eine letzte mcu frage noch. Für welchen Satz ist Vin diesels Character bekannt?", "I am Groot/ Ich bin Groot"));
+        localQuestions.add(new BuzzerQuestion("10. Gehen wir zurück zu Spiele. Thomas mag ja auch Nintendo. Was hat Nintendo bei ihrer gründung verkauft?", "Hanafuda-Spielkarten"));
+        localQuestions.add(new BuzzerQuestion("11. Nintendo hat auch ein sehr bekanntes Maskotchen in rot. Wie heißt denn das ebenfalls bekannte blaue Maskotchen des Rivalen Sega?", "Sonic the Hedgehog"));
+        localQuestions.add(new BuzzerQuestion("12. Wen hab ich mit rotes Maskottchen gemeint?", "Mario 'Jumpman' Mario"));
+        localQuestions.add(new BuzzerQuestion("13. Und wie heißt sein grüner Bruder?", "Luigi Mario"));
+        localQuestions.add(new BuzzerQuestion("14. Nintendo hat noch einen grünen Character in einer anderen Spiele Serie. Wie heißt seine Feenbegleiterin in Majoras Mask?", "Tael"));
+        localQuestions.add(new BuzzerQuestion("15. Letzte frage: Was wünschen wir thomas heute?", "Alles gute zum Geburtstag!!"));
         this.questions = localQuestions;
         currentIndex = 0;
     }
